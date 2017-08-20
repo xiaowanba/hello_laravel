995c73210a3069149ed4790d8d5d1539ee33a7dd
wonvy <iamwonvy@qq.com>
2017-08-20 02:36:35 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Larfffffffffffffffffavel
+                    hahaha
                 </div>
 
                 <div class="links">
